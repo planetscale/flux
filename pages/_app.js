57152f684847f0908ad432e957802241ae81d661
Shawnxkwang <shawnxkwang@gmail.com>
2020-12-03 14:47:03 -0800

    add mysql2 and some examples

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+      });
+  }, []);
   return <Component {...pageProps} />;
 }
 
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,5 +2,14 @@
 
 export default (req, res) => {
   res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
   res.json({ name: 'John Doe' });
 };
+
+// export default function handler(req, res) {
+//   if (req.method === 'POST') {
+//     // Process a POST request
+//   } else {
+//     // Handle any other HTTP method
+//   }
+// }
