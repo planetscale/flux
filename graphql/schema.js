81bebc8541855751ece408612fe3d70565f6e542
Abhi Vaidyanatha <abhi@planetscale.com>
2020-12-09 18:20:09 -0800

    Finish out API objects and CRUD

--- graphql/schema.js
+++ graphql/schema.js
@@ -23,8 +23,64 @@ const User = objectType({
     t.model.posts();
     t.model.profile();
     t.model.org();
-    t.model.orgId();
     t.model.createdAt();
+  },
+});
+
+const Profile = objectType({
+  name: 'Profile',
+  definition(t) {
+    t.model.id();
+    t.model.bio();
+    t.model.avatar();
+    t.model.user();
+  },
+});
+
+const Lens = objectType({
+  name: 'Lens',
+  definition(t) {
+    t.model.id();
+    t.model.name();
+    t.model.description();
+    t.model.org();
+    t.model.posts();
+  },
+});
+
+const Post = objectType({
+  name: 'Post',
+  definition(t) {
+    t.model.id();
+    t.model.createdAt();
+    t.model.name();
+    t.model.summary();
+    t.model.published();
+    t.model.tags();
+    t.model.author();
+    t.model.lens();
+    t.model.replies();
+    t.model.content();
+  },
+});
+
+const Reply = objectType({
+  name: 'Reply',
+  definition(t) {
+    t.model.id();
+    t.model.createdAt();
+    t.model.post();
+    t.model.author();
+    t.model.content();
+  },
+});
+
+const Tag = objectType({
+  name: 'Tag',
+  definition(t) {
+    t.model.id();
+    t.model.name();
+    t.model.posts();
   },
 });
 
@@ -33,19 +89,38 @@ const Query = queryType({
     t.crud.user();
     t.crud.org();
     t.crud.orgs();
+    t.crud.post();
+    t.crud.posts();
+    t.crud.lens();
+    t.crud.lenses();
+    t.crud.replies();
+    t.crud.tags();
   },
 });
 
 const Mutation = mutationType({
   definition(t) {
     t.crud.createOneUser();
-    t.crud.deleteOneUser();
     t.crud.updateOneUser();
+    t.crud.deleteOneUser();
+    t.crud.createOneOrg();
+    t.crud.updateOneOrg();
+    t.crud.deleteOneOrg();
+    t.crud.createOneLens();
+    t.crud.updateOneLens();
+    t.crud.deleteOneLens();
+    t.crud.createOnePost();
+    t.crud.updateOnePost();
+    t.crud.deleteOnePost();
+    t.crud.createOneReply();
+    t.crud.updateOneReply();
+    t.crud.deleteOneReply();
+    t.crud.createOneTag();
   },
 });
 
 export const schema = makeSchema({
-  types: [User, Org, Query, Mutation],
+  types: [User, Org, Profile, Lens, Post, Reply, Tag, Query, Mutation],
   plugins: [nexusPrisma({ experimentalCRUD: true })],
   outputs: {
     schema: path.join(process.cwd(), 'generated', 'schema.graphql'),
