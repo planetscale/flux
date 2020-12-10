618d45264f277d0c57304c345ac9a2dec718cb82
Shawnxkwang <shawnxkwang@gmail.com>
2020-12-10 15:06:19 -0800

    Post Page

---
+++ components/NamePlate/CommenterNamePlate.js
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+  line-height: 17px;
+  margin: 0 0 12px 0;
+
+  > span:nth-of-type(2) {
+    opacity: 0.5;
+  }
+`;
+
+export default function CommenterNamePlate({
+  displayName = 'Abhi Vaidyanatha',
+  userHandle = 'abhi',
+  ...props
+}) {
+  return (
+    <Wrapper>
+      <span>{displayName} </span>
+      <span>{userHandle}</span>
+    </Wrapper>
+  );
+}
