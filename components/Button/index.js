03dba8ad61683497f29cf28ba4ca0bfe595d4085
Shawnxkwang <shawnxkwang@gmail.com>
2020-12-11 13:22:14 -0800

    some UI misc

---
+++ components/Button/index.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const ButtonBase = styled.button`
+  border: none;
+  background-color: unset;
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+`;
--- components/UserIcon/index.js
+++ components/UserIcon/index.js
@@ -1,22 +1,8 @@
 import styled from '@emotion/styled';
 
-const Wrapper = styled.div`
-  img {
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  }
+export const UserIcon = styled.img`
+  border-radius: 50%;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
-// TODO: discard the wrapper, only create a styled.img emotion component, and change the reference to the UserIcon component accordingly
-export default function UserIcon({
-  img = '/user_profile_icon.png',
-  className,
-}) {
-  return (
-    <Wrapper className={className}>
-      <img src={img} alt="user profile avatar" />
-    </Wrapper>
-  );
-}
+export default UserIcon;
