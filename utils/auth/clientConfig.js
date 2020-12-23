8e050cd2d08e9b060ce6af790ddf83f4b78cd901
Shawnxkwang <shawnxkwang@gmail.com>
2020-12-23 12:27:42 -0800

    cors

--- utils/auth/clientConfig.js
+++ utils/auth/clientConfig.js
@@ -11,8 +11,6 @@ const firebaseConfig = {
 };
 
 const googleProvider = new firebase.auth.GoogleAuthProvider();
-console.log(firebaseConfig);
-console.log(process.env);
 
 if (!firebase.apps.length) {
   try {
