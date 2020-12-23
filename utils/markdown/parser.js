e8236b342d4d98c1a9b4043c5e1a9e578a7f6e30
Shawnxkwang <shawnxkwang@gmail.com>
2020-12-22 16:32:18 -0800

    add logic to handle file parsing

--- utils/markdown/parser.js
+++ utils/markdown/parser.js
@@ -1,22 +1,23 @@
 const showdown = require('showdown');
 const matter = require('gray-matter');
+import './options';
 
-export function parseMarkdown({ md }) {
+export function parseMarkdown(md) {
   const converter = new showdown.Converter();
   const html = converter.makeHtml(md);
-  const parsedMeta = matter(converter.getMetadata());
+  const parsedMeta = matter(md);
 
-  if (!parsedMeta.hasOwnProperty('title')) {
+  if (!parsedMeta.data.hasOwnProperty('title')) {
     return console.error('Title metadata not found in post.');
   }
-  if (!parsedMeta.hasOwnProperty('summary')) {
+  if (!parsedMeta.data.hasOwnProperty('summary')) {
     return console.error('Summary metadata not found in post.');
   }
 
   return {
     content: html,
-    title: parsedMeta['title'],
-    summary: parsedMeta['summary'],
-    tags: parsedMeta['tags'],
+    title: parsedMeta.data['title'],
+    summary: parsedMeta.data['summary'],
+    tags: parsedMeta.data['tags'] ?? '',
   };
 }
