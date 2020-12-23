2a235c722a29ef7dcdde7b2cd8c45cbe2774f9d1
Shawnxkwang <shawnxkwang@gmail.com>
2020-12-23 13:46:21 -0800

    remove console

--- graphql/permissions.js
+++ graphql/permissions.js
@@ -6,21 +6,19 @@ const rules = {
     const authHeader = ctx.req.headers.authorization;
     let token = '';
     let userId = '';
-    console.log('authHeader:', authHeader);
+
     if (authHeader?.startsWith('Bearer ')) {
       const tokenArray = authHeader.split(' ');
       // extract the JWT, tokenArray[0] is 'Bearer '
       token = tokenArray[1];
 
       try {
-        userId = await getUserId(token);
         console.log('userId', userId);
       } catch (e) {
         console.error('Get User Id error: ', e);
       }
     }
 
-    console.log('has userId?', Boolean(userId));
     return Boolean(userId);
   }),
 };
