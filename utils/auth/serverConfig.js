e84dd9ed6b5af6b5bc9a4a956fe0ce19ada48fc7
Shawnxkwang <shawnxkwang@gmail.com>
2020-12-23 14:09:43 -0800

    oops

--- utils/auth/serverConfig.js
+++ utils/auth/serverConfig.js
@@ -1,9 +1,9 @@
 import * as admin from 'firebase-admin';
-console.log(typeof process.env.ADMIN_KEY, process.env.ADMIN_KEY);
+console.log(typeof process.env.FIRE_ADMIN_KEY, process.env.FIRE_ADMIN_KEY);
 console.log(
-  typeof JSON.parse(process.env.ADMIN_KEY),
+  typeof JSON.parse(process.env.FIRE_ADMIN_KEY),
   '\n',
-  JSON.parse(process.env.ADMIN_KEY)
+  JSON.parse(process.env.FIRE_ADMIN_KEY)
 );
 if (!admin.apps.length) {
   try {
