85d1e140577e59f7344dec42725d110c2530ef9c
Raunaq Gupta <mail@raunaqgupta.com>
2021-01-08 14:20:01 -0800

    undo

--- pages/_app.js
+++ pages/_app.js
@@ -26,7 +26,7 @@ function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.svg" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&family=Raleway:ital,wght@1,900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Raleway:ital,wght@1,900&display=swap"
           rel="stylesheet"
         ></link>
       </Head>
