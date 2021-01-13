2acb79c97567aef39e1f239d4dcf238f241036e0
William Gordon <gordon@planetscale.com>
2021-01-13 16:24:45 -0500

    Add syntax highlighting

---
+++ components/MarkdownEditor/CodeBlock.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+export default function CodeBlock({ language, value }) {
+  return (
+    <SyntaxHighlighter language={language} style={tomorrow}>
+      {value}
+    </SyntaxHighlighter>
+  );
+}
