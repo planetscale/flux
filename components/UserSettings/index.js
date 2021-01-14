b7bc764a85ce2ebafa460a1ce432d74ceeada975
Raunaq Gupta <mail@raunaqgupta.com>
2021-01-14 11:53:23 -0800

    dont want to stash

--- components/UserSettings/index.js
+++ components/UserSettings/index.js
@@ -1,22 +1,23 @@
 import styled from '@emotion/styled';
 import UserIcon from 'components/UserIcon';
-import Logout from './Logout';
+import { useAuthActions } from 'state/auth';
 
-const UserSettingsWrapper = styled.div`
+const UserSettingsWrapper = styled.ul`
   width: 360px;
   background: var(--background);
   border-radius: 4px;
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
-  padding: 24px;
+  padding: 0 0 8px;
+  list-style: none;
 `;
 
-const UserInfo = styled.div`
+const UserInfo = styled.li`
   display: flex;
   flex-direction: row;
   align-items: center;
-  margin-bottom: 24px;
+  padding: 24px;
 
   > img {
     width: 42px;
@@ -41,22 +42,53 @@ const UserNickname = styled.p`
   color: #666;
 `;
 
+const MenuItem = styled.li`
+  border-bottom: 1px solid var(--accent2);
+`;
+
+const MenuAction = styled.a`
+  display: block;
+  color: var(--foreground);
+  padding: 24px;
+
+  &:hover {
+    cursor: pointer;
+    background-color: var(--highlight);
+    color: white;
+  }
+`;
+
 export default function UserSettings({
   profileImg,
   displayName,
   userHandle,
   ...props
 }) {
+  const { userLogout } = useAuthActions();
+
+  const handleLogout = () => {
+    userLogout();
+  };
+
   return (
     <UserSettingsWrapper>
-      <UserInfo>
-        <UserIcon src={profileImg} alt={`${displayName}'s avatar`} />
-        <UserNameContainer>
-          <UserName>{displayName}</UserName>
-          <UserNickname>@{userHandle}</UserNickname>
-        </UserNameContainer>
-      </UserInfo>
-      <Logout />
+      <MenuItem>
+        <UserInfo>
+          <UserIcon src={profileImg} alt={`${displayName}'s avatar`} />
+          <UserNameContainer>
+            <UserName>{displayName}</UserName>
+            <UserNickname>@{userHandle}</UserNickname>
+          </UserNameContainer>
+        </UserInfo>
+      </MenuItem>
+      <MenuItem>
+        <MenuAction>Mood</MenuAction>
+      </MenuItem>
+      <MenuItem>
+        <MenuAction type="button" onClick={handleLogout}>
+          Log Out
+        </MenuAction>
+      </MenuItem>
     </UserSettingsWrapper>
   );
 }
