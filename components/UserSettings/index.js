44589439f54573e736cde4f3329208dab656851c
Raunaq Gupta <mail@raunaqgupta.com>
2021-01-15 11:20:43 -0800

    implement mood switch

--- components/UserSettings/index.js
+++ components/UserSettings/index.js
@@ -1,6 +1,29 @@
 import styled from '@emotion/styled';
 import UserIcon from 'components/UserIcon';
 import { useAuthActions } from 'state/auth';
+import { getTheme, setTheme } from 'pageUtils/post/theme';
+import * as RadioGroup from '@radix-ui/react-radio-group';
+
+const Icon = styled.span`
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+  background: var(--foreground);
+  mask-size: cover;
+  z-index: 1;
+
+  &.icon-system {
+    mask: url('/icon_system.svg');
+  }
+
+  &.icon-light {
+    mask: url('/icon_light.svg');
+  }
+
+  &.icon-dark {
+    mask: url('/icon_dark.svg');
+  }
+`;
 
 const UserSettingsWrapper = styled.ul`
   width: 360px;
@@ -13,7 +36,7 @@ const UserSettingsWrapper = styled.ul`
   list-style: none;
 `;
 
-const UserInfo = styled.li`
+const UserInfo = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -42,12 +65,24 @@ const UserNickname = styled.p`
   color: #666;
 `;
 
-const MenuItem = styled.li`
+const MenuItemContainer = styled.li`
   border-bottom: 1px solid var(--accent2);
 `;
 
+const MenuItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  color: var(--foreground);
+  padding: 24px;
+`;
+
 const MenuAction = styled.a`
-  display: block;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
   color: var(--foreground);
   padding: 24px;
 
@@ -58,6 +93,47 @@ const MenuAction = styled.a`
   }
 `;
 
+const StyledRadioGroup = styled(RadioGroup.Root)`
+  border: 1px solid var(--accent2);
+`;
+
+const StyledRadio = styled(RadioGroup.Item)`
+  appearance: none;
+  background-color: var(--background);
+  border: none;
+  padding: 8px;
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+
+  & ~ & {
+    border-left: 1px solid var(--accent2);
+  }
+
+  &:first-child {
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+  }
+
+  &:focus {
+    outline: 'none';
+    box-shadow: 'inset 0 0 0 1px dodgerblue, 0 0 0 1px dodgerblue';
+  }
+`;
+
+const StyledIndicator = styled(RadioGroup.Indicator)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: #c56a86;
+
+  & ~ ${Icon} {
+    background: white;
+  }
+`;
+
 export default function UserSettings({
   profileImg,
   displayName,
@@ -65,14 +141,19 @@ export default function UserSettings({
   ...props
 }) {
   const { userLogout } = useAuthActions();
+  const currentTheme = getTheme();
 
   const handleLogout = () => {
     userLogout();
   };
 
+  const handleThemeChange = value => {
+    setTheme(value.target.value);
+  };
+
   return (
     <UserSettingsWrapper>
-      <MenuItem>
+      <MenuItemContainer>
         <UserInfo>
           <UserIcon src={profileImg} alt={`${displayName}'s avatar`} />
           <UserNameContainer>
@@ -80,15 +161,34 @@ export default function UserSettings({
             <UserNickname>@{userHandle}</UserNickname>
           </UserNameContainer>
         </UserInfo>
-      </MenuItem>
-      <MenuItem>
-        <MenuAction>Mood</MenuAction>
-      </MenuItem>
-      <MenuItem>
+      </MenuItemContainer>
+      <MenuItemContainer>
+        <MenuItem>
+          <div>Mood</div>
+          <StyledRadioGroup
+            defaultValue={currentTheme}
+            onValueChange={handleThemeChange}
+          >
+            <StyledRadio value="system">
+              <StyledIndicator />
+              <Icon className="icon-system"></Icon>
+            </StyledRadio>
+            <StyledRadio value="light">
+              <StyledIndicator />
+              <Icon className="icon-light"></Icon>
+            </StyledRadio>
+            <StyledRadio value="dark">
+              <StyledIndicator />
+              <Icon className="icon-dark"></Icon>
+            </StyledRadio>
+          </StyledRadioGroup>
+        </MenuItem>
+      </MenuItemContainer>
+      <MenuItemContainer>
         <MenuAction type="button" onClick={handleLogout}>
           Log Out
         </MenuAction>
-      </MenuItem>
+      </MenuItemContainer>
     </UserSettingsWrapper>
   );
 }
--- pageUtils/post/theme.js
+++ pageUtils/post/theme.js
@@ -4,34 +4,81 @@ const theme = {
   sizes: {
     tablet: 900,
     phone: 750,
-    maxWidth: '1170px',
+    maxWidth: '90ch',
   },
 
   colorMode: {
     light: {
-      background1: '#fff',
-      foreground1: '#000',
-      accent: '#eee',
-      accent2: '#ccc',
+      background: '#fff',
+      foreground: '#000',
+      accent: '#ccc',
+      accent2: '#eee',
+      accent3: '#f7f7f7',
       link: '#007BC7',
-      text: '#555',
-      brightness: 'brightness(100%)',
-      shadow1:
+      text: 'rgb(60, 66, 87)',
+      shadow:
         '0px 0px 4px rgba(0, 0, 0, 0.04), 0px 4px 13px rgba(0, 0, 0, 0.08)',
+      highlight: '#c56a86',
     },
     dark: {
-      background1: '#090909',
-      foreground1: 'rgba(255, 255, 255, 0.9)',
-      accent: '#1e1e1e',
-      accent2: '#333',
+      background: '#090909',
+      foreground: 'rgba(255, 255, 255, 0.9)',
+      accent: '#666',
+      accent2: '#1e1e1e',
+      accent3: '#0c0c0c',
       link: '#009AFA',
       text: '#ccc',
-      brightness: 'brightness(85%)',
-      shadow1:
+      shadow:
         '0px 0px 4px rgba(0, 0, 0, 0.04), 0px 4px 13px rgba(0, 0, 0, 0.08)',
+      highlight: '#c56a86',
     },
   },
 };
+
+export function initTheme() {
+  if (getTheme() === null) {
+    localStorage.setItem('theme', 'system');
+  } else {
+    setTheme(getTheme());
+  }
+}
+
+export function getTheme() {
+  return localStorage.getItem('theme');
+}
+
+export function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  let root = typeof document !== 'undefined' && document.documentElement;
+
+  if (themeName === 'system') {
+    root.style.removeProperty('--background');
+    root.style.removeProperty('--foreground');
+    root.style.removeProperty('--accent');
+    root.style.removeProperty('--accent2');
+    root.style.removeProperty('--accent3');
+    root.style.removeProperty('--link');
+    root.style.removeProperty('--text');
+    root.style.removeProperty('--shadow');
+    root.style.removeProperty('--highlight');
+  } else {
+    root.style.setProperty(
+      '--background',
+      theme.colorMode[themeName].background
+    );
+    root.style.setProperty(
+      '--foreground',
+      theme.colorMode[themeName].foreground
+    );
+    root.style.setProperty('--accent', theme.colorMode[themeName].accent);
+    root.style.setProperty('--accent2', theme.colorMode[themeName].accent2);
+    root.style.setProperty('--accent3', theme.colorMode[themeName].accent3);
+    root.style.setProperty('--link', theme.colorMode[themeName].link);
+    root.style.setProperty('--text', theme.colorMode[themeName].text);
+    root.style.setProperty('--shadow', theme.colorMode[themeName].shadow);
+    root.style.setProperty('--highlight', theme.colorMode[themeName].highlight);
+  }
+}
 
 export const media = Object.keys(theme.sizes).reduce((accumulator, label) => {
   const emSize = theme.sizes[label] / 16;
