69c3938ad547c748c820a7623eff601591c60407
William Gordon <gordon@planetscale.com>
2021-02-01 16:08:18 -0500

    Use ajax and plain mysql instead of graphql and prisma

---
+++ pages/test/index.js
@@ -0,0 +1,148 @@
+import useSWR from 'swr';
+import { defaultFetchHeaders } from 'utils/auth/clientConfig';
+import PostList from 'components/PostList';
+import { useTopBarActions, useTopBarContext } from 'state/topBar';
+import { useEffect, useRef, useState } from 'react';
+import { useUserContext } from 'state/user';
+import styled from '@emotion/styled';
+import { useBottomScrollListener } from 'react-bottom-scroll-listener';
+import { useImmer } from 'use-immer';
+import { media } from 'pageUtils/post/theme';
+
+const fetcher = async (url, last, before, _) => {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+      Authorization: defaultFetchHeaders.authorization,
+    },
+  });
+  return response.json();
+};
+
+const HomeWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  ${media.phone`
+    width: 100%;
+  `}
+`;
+
+// TODO: replace this hack with backend implementation
+function getLensPosts(lenses, subHeader) {
+  if (!lenses || lenses.length === 0) {
+    return [];
+  }
+
+  if (subHeader.toLowerCase() === 'posts') {
+    return lenses.flatMap(lens => lens.posts);
+  }
+
+  const lens = lenses.find(lens => {
+    return lens.name !== undefined && lens.name === subHeader;
+  });
+
+  if (lens === undefined) {
+    return [];
+  }
+
+  return lens.posts;
+}
+
+const DEFAULT_PAGE_ADDEND = 10;
+
+const LOADING_POSTS = Array(10).fill(null);
+
+export default function Home({ href, ...props }) {
+  const [state, setState] = useImmer({
+    postList: {},
+    last: DEFAULT_PAGE_ADDEND,
+    before: -1,
+    forceFetch: 0,
+  });
+
+  const { data, error } = useSWR(
+    [
+      '/api/get-posts',
+      defaultFetchHeaders.authorization,
+      state.last,
+      state.before,
+      state.forceFetch,
+    ],
+    fetcher,
+    {
+      onSuccess: data => {
+        const mappedPosts = data.reduce((acc, curr) => {
+          acc[curr.id] = { ...curr };
+          return acc;
+        }, {});
+
+        setState(draft => {
+          draft.postList = { ...state.postList, ...mappedPosts };
+        });
+      },
+    }
+  );
+  const { setHeaders, setTag } = useTopBarActions();
+  const { user } = useUserContext();
+  const { subHeader, selectedTag } = useTopBarContext();
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (user?.org?.name) {
+      setHeaders({
+        header: user?.org.name,
+      });
+    }
+  }, [user?.org]);
+
+  // This effect will handle triggering the fetchPost effect when the tag is changed.
+  useEffect(() => {
+    setLoading(true);
+    // The useBottomScrollListener will fire unecessarily if we are still scrolled to the bottom as we reset the post list.
+    window.scrollTo(0, 0);
+    setState(draft => {
+      draft.postList = {};
+      draft.before = -1;
+      draft.forceFetch = draft.forceFetch + 1;
+    });
+  }, [selectedTag]);
+
+  useBottomScrollListener(
+    () => {
+      const postLength = Object.keys(state.postList).length;
+      if (postLength && !isLoading) {
+        setState(draft => {
+          // We can garuntee the object is sorted by id ASC, so the oldest post is first element
+          draft.before = Number(Object.keys(state.postList)[0]);
+        });
+      }
+    },
+    {
+      offset: 100,
+      debounce: 300,
+      DebounceOptions: { leading: false },
+    }
+  );
+
+  const handleTagClick = (e, tagName) => {
+    e.stopPropagation();
+
+    if (tagName) {
+      setTag(tagName);
+    }
+  };
+
+  return (
+    <HomeWrapper>
+      <PostList
+        posts={[
+          ...Object.values(state.postList).reverse(),
+          ...(isLoading ? LOADING_POSTS : []),
+        ]}
+        handleTagClick={handleTagClick}
+      />
+    </HomeWrapper>
+  );
+}
