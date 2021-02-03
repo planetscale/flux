1ec685bcaa5dc37caef1845edd20cdc4bdffdd2e
William Gordon <gordon@planetscale.com>
2021-02-03 18:35:37 -0500

    More stuff

--- pages/api/get-posts.js
+++ pages/api/get-posts.js
@@ -1,55 +1,10 @@
 import mysql from 'mysql2/promise';
-import Cors from 'cors';
-import { decodeToken } from 'utils/auth/serverConfig';
+import { cors, validateUser } from './_utils/middleware';
 
-const cors = Cors({
-  methods: ['GET', 'POST', 'HEAD'],
-});
-
-const validateUser = async (req, res, callbackFn) => {
-  const authHeader = req.headers.authorization;
-  let token = '';
-  let decodedToken = '';
-
-  if (authHeader?.startsWith('Bearer ')) {
-    const tokenArray = authHeader.split(' ');
-    // extract the JWT, tokenArray[0] is 'Bearer '
-    token = tokenArray[1];
-
-    try {
-      decodedToken = await decodeToken(token);
-      callbackFn(
-        Boolean(decodedToken.uid) &&
-          Boolean(
-            decodedToken.email.match(
-              new RegExp(process.env.NEXT_PUBLIC_ALLOWED_EMAIL_REGEX)
-            )
-          )
-      );
-    } catch (e) {
-      callbackFn(e);
-    }
-  }
-};
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, result => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
-    });
-  });
-}
-
-// This is a simple database connection test to prove you can connect to a persistent store for your application.
-export default async (req, res) => {
+module.exports = async (req, res) => {
   try {
-    await runMiddleware(req, res, cors);
-    await runMiddleware(req, res, validateUser);
+    cors(req, res);
+    await validateUser(req);
   } catch (e) {
     res.status(401).json({ error: e.toString() });
     return;
--- state/user/index.js
+++ state/user/index.js
@@ -40,18 +40,20 @@ const useUserActions = () => {
           Authorization: defaultFetchHeaders.authorization,
         },
       });
-      const user = await response.json();
+      const { data: user } = await response.json();
       // FIXME: Refactor the app's user object to not have nested elements
-      const restructureUser = {
-        ...user,
-        profile: {
-          avatar: user.avatar,
-        },
-        org: {
-          id: user.orgId,
-          name: user.orgName,
-        },
-      };
+      const restructureUser = user
+        ? {
+            ...user,
+            profile: {
+              avatar: user.avatar,
+            },
+            org: {
+              id: user.orgId,
+              name: user.orgName,
+            },
+          }
+        : null;
 
       updateState(draft => {
         draft.user = restructureUser;
@@ -67,7 +69,41 @@ const useUserActions = () => {
     }
   };
 
-  return { getUser };
+  const createUser = async params => {
+    const res = await fetch(`/api/create-user`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        Authorization: defaultFetchHeaders.authorization,
+      },
+      body: JSON.stringify(params),
+    });
+    const { data: user, error } = await res.json();
+    if (!error) {
+      // FIXME: Refactor the app's user object to not have nested elements
+      const restructureUser = user
+        ? {
+            ...user,
+            profile: {
+              avatar: user.avatar,
+            },
+            org: {
+              id: user.orgId,
+              name: user.orgName,
+            },
+          }
+        : null;
+      updateState(draft => {
+        draft.user = restructureUser;
+        draft.loading = false;
+        draft.loaded = true;
+      });
+    } else {
+      console.error(e);
+    }
+  };
+
+  return { getUser, createUser };
 };
 
 export { UserContextProvider, useUserContext, useUserActions };
