e19abba358b3ff46b4a3fc1d8f264824bfb707dd
Shawn Wáng /wɔŋ/ <Shawnxkwang@users.noreply.github.com>
2021-02-09 18:22:53 -0800

    Add custom document file (#357)

---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class AppDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="Flux" content="black-translucent"></meta>
+          <link rel="icon" href="/favicon.svg" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link rel="manifest" href="/manifest.json"></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Raleway:ital,wght@1,900&display=swap"
+            rel="preload"
+            as="style"
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Raleway:ital,wght@1,900&display=swap"
+            rel="stylesheet"
+            media="print"
+            onLoad="this.media='all'"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default AppDocument;
