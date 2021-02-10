fef33d1d69fbdcf14fe05367006b34cc77c9efa0
Shawnxkwang <shawnxkwang@gmail.com>
2021-02-09 17:34:53 -0800

    update head

--- pages/_app.js
+++ pages/_app.js
@@ -26,24 +26,6 @@ function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Flux</title>
-        <link rel="icon" href="/favicon.svg" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link rel="manifest" href="/manifest.json"></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Raleway:ital,wght@1,900&display=swap"
-          rel="preload"
-          as="style"
-        ></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Raleway:ital,wght@1,900&display=swap"
-          rel="stylesheet"
-          media="print"
-          onLoad="this.media='all'"
-        ></link>
       </Head>
       <Global
         styles={css`
