bc0f222b55d70f125a91cc3dcef599f05e8f117a
rawknuckles <mail@raunaqgupta.com>
2021-03-02 16:03:19 -0800

    Update optional summary regex (#406)

--- pages/api/create-post.js
+++ pages/api/create-post.js
@@ -2,7 +2,8 @@ import { cors, runMiddleware, validateUser } from './_utils/middleware';
 import { createConnection } from './_utils/connection';
 import slackNotification from './_utils/notifications/slack';
 
-const NEW_LINE_REGEX = /\n+|(\\\n)+/gm;
+const REMOVE_NEWLINE = /\n+|\\n+|\\\n+/gm;
+const REMOVE_DOUBLESPACE = /  +/gm;
 
 // This is a simple database connection test to prove you can connect to a persistent store for your application.
 module.exports = async (req, res) => {
@@ -19,7 +20,11 @@ module.exports = async (req, res) => {
 
   let summary = initSummary;
   if (!initSummary) {
-    summary = `${content.substr(0, 60).replace(NEW_LINE_REGEX, ' ')}...`;
+    summary = `${content
+      .substr(0, 60)
+      .replace(REMOVE_NEWLINE, ' ')
+      .replace(REMOVE_DOUBLESPACE, ' ')}...`;
+    console.log(summary);
   }
 
   const connection = await createConnection();
