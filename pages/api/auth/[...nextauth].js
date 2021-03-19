d6e44bd46457867bff632fd3d5310360fd8db7e0
William Gordon <gordon@planetscale.com>
2021-03-19 12:01:13 -0400

    Remove old google auth provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,15 +4,6 @@ import Providers from 'next-auth/providers';
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      state: false,
-      pages: {
-        signIn: '/',
-        signOut: '/',
-      },
-    }),
     Providers.Credentials({
       name: 'NoAuthEmail',
       credentials: {
