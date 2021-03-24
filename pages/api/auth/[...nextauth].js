b4568ca2ba297c18c14b5b6d94e3a4a3415bee67
Raunaq Gupta <mail@raunaqgupta.com>
2021-03-24 13:13:20 -0700

    Remove google provider creds

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -14,15 +14,6 @@ export default NextAuth({
         return credentials.email ? { email: credentials.email } : null;
       },
     }),
-    Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      state: false,
-      pages: {
-        signIn: '/',
-        signOut: '/',
-      },
-    }),
   ],
   pages: {
     signIn: '/',
