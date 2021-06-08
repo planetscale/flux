bd7d86043b79a94dce72a3ec9605af59cc620a32
Raunaq Gupta <mail@raunaqgupta.com>
2021-06-08 13:28:20 -0700

    Remove console statement

--- authentication/components/Google.js
+++ authentication/components/Google.js
@@ -3,7 +3,6 @@ import { Google as RemixLineGoogle } from '@styled-icons/remix-line';
 import { ButtonWireframe } from 'components/Button';
 
 export default function Google({ isPrimaryOAuth }) {
-  console.log(isPrimaryOAuth);
   const handleLogin = () => {
     signIn('google', { callbackUrl: '/' });
   };
